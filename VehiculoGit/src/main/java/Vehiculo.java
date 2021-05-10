cb855e0d952f2b32a78ef1bf0e28570989898e14
DAM1_Alu02 <DAM1_Alu02@SMR1-PC04.smr.ald>
2021-05-10 20:23:28 +0200

    Estado inicial do proxecto

---
+++ VehiculoGit/src/main/java/Vehiculo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DAM1_Alu02
+ */
+public class Vehiculo {
+    public static void main(String[] args) {
+        
+    }
+}
